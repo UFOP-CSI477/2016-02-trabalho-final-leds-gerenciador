119b65679e111cb1b99f863a3642243560960348
saraiva3 <lucassaraiva12@yahoo.com.br>
2017-03-07 16:51:17 -0300

    Part 1 Code
    Para funcionar, cole a pasta vendor em seu repositorio local

---
+++ source/resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Sistema Academico - Web 2016.2
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
